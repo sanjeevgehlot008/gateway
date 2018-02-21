d8a098a44c6435bebca6dbe21915f3d0917e834f
sanjeev gehlot <sanjeev.gehlot@mindcrewtech.com>
2018-02-21 18:34:38 +0530

    FFC

---
+++ model/connection.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+//mongoose.connect('mongodb://userNPB:7KWwgoSkwuKam5II@mongodb/nodewebappdbwebsocket');
+//mongoose.connect(process.env.DATABASE_URL);
+mongodb://userHM1:rGXYNI3aLGnwYqpK@mongodb/sampledb');
+var connectionOne = mongoose.connection;
+module.exports = connectionOne;
